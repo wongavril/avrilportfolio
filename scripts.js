86cb73416f262ccc6a8ac4cca2b792c28f42f2f5
Avril <wongavril@gmail.com>
2019-02-25 16:51:21 -0800

    scripts added

---
+++ scripts.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+  /* STICKY LOGIC */
+  const navbar = document.getElementById("navbar");
+  const mainContent = document.getElementById("mainContent");
+
+  /*const sticky = navbar.offsetTop;*/
+
+  function applySticky() {
+    if (window.pageYOffset > sticky) {
+      navbar.classList.add("sticky");
+      mainContent.classList.add("smoothContent");
+    } else {
+      navbar.classList.remove("sticky");
+      mainContent.classList.remove("smoothContent");
+    }
+  }
+
+  window.onscroll = function() {applySticky()};
+
+
+  /* HAMBURGER MENU LOGIC */
+  const icon = document.getElementById("menuIcon");
+  const menu = document.getElementById("extendedMenu");
+
+  icon.addEventListener("click", () => {
+    if (icon.classList.contains("rotate90")) {
+      icon.classList.remove("rotate90");
+      menu.classList.remove("show");
+      menu.classList.add("hide");
+    } else {
+      icon.classList.add("rotate90");
+      menu.classList.remove("hide");
+      menu.classList.add("show");
+    }
+  })
+});
