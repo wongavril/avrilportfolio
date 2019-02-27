c2e58c864a4e6bd1a1c5e5485d29b89ab4ea041f
Avril <wongavril@gmail.com>
2019-02-27 13:50:09 -0800

    responsive layout

--- scriptsProject.js
+++ scriptsProject.js
@@ -5,6 +5,20 @@ document.addEventListener("DOMContentLoaded", function() {
   const navbar = document.getElementById("navbar");
   const mainContent = document.getElementById("mainContent");
 
+  const sticky = navbar.offsetTop;
+
+  function applySticky() {
+    if (window.pageYOffset > sticky) {
+      navbar.classList.add("sticky");
+      mainContent.classList.add("smoothContent");
+    } else {
+      navbar.classList.remove("sticky");
+      mainContent.classList.remove("smoothContent");
+    }
+  }
+
+  window.onscroll = function() {applySticky()};
+
 
   /* HAMBURGER MENU LOGIC */
   const icon = document.getElementById("menuIcon");
