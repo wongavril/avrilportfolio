92cb3370670908410ccff14a28a059f1951d0a16
Avril <wongavril@gmail.com>
2019-03-11 23:11:34 -0700

    minor changes

--- scripts.js
+++ scripts.js
@@ -18,26 +18,3 @@ document.addEventListener("DOMContentLoaded", function() {
   })
 
 });
-
-
-  /* SCROLL ANIMATIONS */
-  $(document).ready(function() {
-    // Check if element is scrolled into view
-    function isScrolledIntoView(elem) {
-      var docViewTop = $(window).scrollTop();
-      var docViewBottom = docViewTop + $(window).height();
-
-      var elemTop = $(elem).offset().top;
-      var elemBottom = elemTop + $(elem).height();
-
-      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-    }
-    // If element is scrolled into view, fade it in
-    $(window).scroll(function() {
-      $('.scroll-animations .animated').each(function() {
-        if (isScrolledIntoView(this) === true) {
-          $(this).addClass('fadeInUp');
-        }
-      });
-    });
-  });
